b87eb4f119b54748a3d5adf37196d2428f24c701
Desnecesauron <cesinhasampa63@gmail.com>
2024-04-04 01:15:40 -0300

    Update project files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,137 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  ElementRef,
+  EventEmitter, Inject,
+  OnInit,
+  Output, PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
+import {NgxCaptureService} from 'ngx-capture';
+import {WebcamImage, WebcamInitError, WebcamModule, WebcamUtil} from 'ngx-webcam';
+import {Observable, Subject, tap} from 'rxjs';
+import QrScanner from 'qr-scanner';
+import {RouterOutlet} from '@angular/router';
+import {isPlatformBrowser, JsonPipe, NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe, NgForOf, NgIf, WebcamModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ARproject';
+export class AppComponent implements OnInit {
+  title = 'ARProject';
+  @ViewChild('webCam', {static: false}) webCam?: ElementRef<any>;
+
+  @Output()
+  public pictureTaken = new EventEmitter<WebcamImage>();
+
+  private sla: boolean = false;
+  public showWebcam = true;
+  public allowCameraSwitch = true;
+  public multipleWebcamsAvailable = true;
+  public deviceId?: string;
+  public videoOptions: MediaTrackConstraints = {};
+  public errors: WebcamInitError[] = [];
+  private trigger: Subject<void> = new Subject<void>();
+  private nextWebcam: Subject<boolean | string> = new Subject<
+    boolean | string
+  >();
+  public screenWidth?: number;
+  public screenHeight?: number;
+  public resultQrCode?: string;
+
+  constructor(
+    private readonly captureService: NgxCaptureService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+  }
+/*
+  public ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+
+    if (isPlatformBrowser(this.platformId)) {
+      WebcamUtil.getAvailableVideoInputs().then(
+        (mediaDevices: MediaDeviceInfo[]) => {
+          this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+        }
+      );
+    }
+
+    this.triggerSnapshotAsync();
+  }
+*/
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.sla = true;
+    }, 1000)
+    if (isPlatformBrowser(this.platformId)) {
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+
+      WebcamUtil.getAvailableVideoInputs().then(
+        (mediaDevices: MediaDeviceInfo[]) => {
+          this.multipleWebcamsAvailable = mediaDevices && mediaDevices.length > 1;
+        }
+      );
+
+      this.triggerSnapshotAsync();
+    }
+  }
+
+  public triggerSnapshotAsync() {
+    // Wait 3 seconds and then take a picture each 1 second
+    setTimeout(() => {
+      setInterval(() => {
+        console.log('Taking picture');
+        this.triggerSnapshot();
+      }, 1000);
+    }, 3000);
+  }
+
+  public triggerSnapshot(): void {
+    this.captureService
+      .getImage(this.webCam!.nativeElement as any, true)
+      .pipe(
+        tap((img) => {
+          QrScanner.scanImage(img)
+            .then((result) => {
+              console.log(result)
+              this.resultQrCode = result;
+            })
+            .catch((error) => console.log(error || 'No QR code found.'));
+        })
+      )
+      .subscribe();
+  }
+
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+
+
+  public handleImage(webcamImage: WebcamImage): void {
+    this.pictureTaken.emit(webcamImage);
+  }
+
+  public cameraWasSwitched(deviceId: string): void {
+    this.deviceId = deviceId;
+  }
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  public get nextWebcamObservable(): Observable<boolean | string> {
+    return this.nextWebcam.asObservable();
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,31 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, NgModule} from '@angular/core';
+import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
+import {NgxCaptureModule} from 'ngx-capture';
+import {WebcamModule} from 'ngx-webcam';
+import {CommonModule} from '@angular/common';
+import {AppComponent} from './app.component';
+import {provideRouter} from "@angular/router";
+import {routes} from "./app.routes";
+
+NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgxCaptureModule,
+    WebcamModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideClientHydration()],
+};
+
+/*import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,3 +33,4 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
+*/
