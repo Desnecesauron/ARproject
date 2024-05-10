5f5011f1608ebd6a931d1c407c386de0c867c362
Desnecesauron <cesinhasampa63@gmail.com>
2024-05-10 07:08:45 -0300

    Adding more items

--- src/app/data-getter.service.ts
+++ src/app/data-getter.service.ts
@@ -20,6 +20,24 @@ export class DataGetterService {
       case 'squirtle':
         result = { image: this.json4.image, text: this.json4.text };
         break;
+      case 'cpu':
+        result = { image: this.json5.image, text: this.json5.text };
+        break;
+      case 'ssd':
+        result = { image: this.json6.image, text: this.json6.text };
+        break;
+      case 'placa-de-som':
+        result = { image: this.json7.image, text: this.json7.text };
+        break;
+      case 'memoria-ram':
+        result = { image: this.json8.image, text: this.json8.text };
+        break;
+      case 'placa-mae':
+        result = { image: this.json9.image, text: this.json9.text };
+        break;
+      case 'placa-de-video':
+        result = { image: this.json10.image, text: this.json10.text };
+        break;
       default:
         result = { image: '', text: '' };
         break;
@@ -48,6 +66,38 @@ export class DataGetterService {
   json4 = {
     "image": "https://raw.githubusercontent.com/Desnecesauron/ARproject/gh-pages/assets/squirtle.png",
     "text": "Squirtle é um Pokémon do tipo água. Ele é um dos Pokémon iniciais da primeira geração. Squirtle é um Pokémon muito amigável e leal, mas também é muito teimoso. Ele é um Pokémon muito forte e pode evoluir para Wartortle e depois para Blastoise. Squirtle é um Pokémon muito popular e é um dos Pokémon mais conhecidos de todos os tempos."
+  }
+
+//cpu.png
+  json5 = {
+    "image": "https://raw.githubusercontent.com/Desnecesauron/ARproject/gh-pages/assets/cpu.png",
+    "text": "O processador é a parte mais importante de um computador. Ele é responsável por executar as instruções e processar os dados. O processador é o cérebro do computador e é responsável por executar todas as tarefas. Existem vários tipos de processadores, como Intel, AMD, entre outros. O processador é uma das partes mais caras de um computador, mas é essencial para o bom funcionamento do computador."
+  }
+
+// ssd.png
+  json6 = {
+    "image": "https://raw.githubusercontent.com/Desnecesauron/ARproject/gh-pages/assets/ssd.png",
+    "text": "O SSD é um tipo de armazenamento de dados que é mais rápido e mais confiável do que o HD tradicional. O SSD é uma ótima opção para quem quer um computador mais rápido e mais eficiente. O SSD é mais caro do que o HD tradicional, mas vale a pena o investimento. O SSD é uma das melhores maneiras de melhorar o desempenho do seu computador."
+  }
+// placa-de-som.png
+  json7 = {
+    "image": "https://raw.githubusercontent.com/Desnecesauron/ARproject/gh-pages/assets/placa-de-som.png",
+    "text": "A placa de som é um dispositivo de hardware que permite ao computador reproduzir e gravar áudio. A placa de som é responsável por converter os sinais de áudio digitais em sinais de áudio analógicos que podem ser reproduzidos por alto-falantes ou fones de ouvido. A placa de som é essencial para quem quer ouvir música, assistir filmes ou jogar jogos no computador."
+  }
+// memoria-ram.png
+  json8 = {
+    "image": "https://raw.githubusercontent.com/Desnecesauron/ARproject/gh-pages/assets/memoria-ram.png",
+    "text": "A memória RAM é um tipo de memória de acesso aleatório que é usada para armazenar dados temporariamente. A memória RAM é essencial para o bom funcionamento do computador, pois é responsável por armazenar os dados que estão sendo usados no momento. A memória RAM é mais rápida do que o HD tradicional e é essencial para quem quer um computador rápido e eficiente."
+  }
+// placa-mae.png
+  json9 = {
+    "image": "https://raw.githubusercontent.com/Desnecesauron/ARproject/gh-pages/assets/placa-mae.png",
+    "text": "A placa-mãe é a principal placa de circuito de um computador. Ela é responsável por conectar todos os componentes do computador, como o processador, a memória RAM, a placa de vídeo, entre outros. A placa-mãe é essencial para o bom funcionamento do computador e é uma das partes mais importantes do computador."
+  }
+// placa-de-video.png
+  json10 = {
+    "image": "https://raw.githubusercontent.com/Desnecesauron/ARproject/gh-pages/assets/placa-de-video.png",
+    "text": "A placa de vídeo é um dispositivo de hardware que é responsável por processar e exibir imagens no computador. A placa de vídeo é essencial para quem quer jogar jogos ou assistir vídeos no computador. Existem vários tipos de placas de vídeo, como Nvidia, AMD, entre outros. A placa de vídeo é uma das partes mais importantes do computador e é essencial para o bom funcionamento do computador."
   }
 
 }
